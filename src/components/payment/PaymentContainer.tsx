145da00d194f42bf09b8c4608f84d9af36766a20
Juan Cruz Valencia Vernengo <jcvalencia@ismt.edu.ar>
2023-05-12 09:44:44 -0300

    stripe CardElement migration to PaymentElement

--- src/components/payment/PaymentContainer.tsx
+++ src/components/payment/PaymentContainer.tsx
@@ -1,53 +1,18 @@
-import { RESPONSE_STATUS } from "@/interfaces";
-import { BrowserRest } from "@/utils/frontend/browser-rest";
 import getStripe from "@/utils/stripe/initStripe";
 import { Elements } from "@stripe/react-stripe-js";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { StripeElementsOptions } from "@stripe/stripe-js";
 
 type Props = {
   children: React.ReactNode;
 };
 
-type StripeResponse = {
-  payload: {
-    client_secret: string;
-  };
-};  
-
 const PaymentContainer: React.FC<Props> = ({ children }) => {
   const stripePromise = getStripe();
-  const { data: session } = useSession();
-  const [stripeScret, setStripeSecret] = useState<string>();
 
-  useEffect(() => {
-    if (session) {
-      const getClientSecret = async () => {
-        const response = await BrowserRest.post<StripeResponse>(
-          "/payment",
-          {
-            total: 200,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${session.user?.token}`,
-            },
-          }
-        );
-
-        if (response.status === RESPONSE_STATUS.SUCCESS) {
-          setStripeSecret(response.data.payload.client_secret);
-        }
-      };
-      getClientSecret();
-    }
-  }, [session]);
-
-  const options = {
-    clientSecret: stripeScret,
-    appearance: {
-      /*...*/
-    },
+  const options: StripeElementsOptions = {
+    mode: "payment",
+    currency: "usd",
+    amount: 1099,
   };
 
   return (
--- src/components/payment/PaymentForm.tsx
+++ src/components/payment/PaymentForm.tsx
@@ -1,19 +1,123 @@
 import {
-  CardElement,
   PaymentElement,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent } from "react";
 import { BrowserRest } from "@/utils/frontend/browser-rest";
-import { RESPONSE_STATUS, STRIPE_STATUS } from "@/interfaces";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-// type StripeResponse = {
-//   payload: {
-//     client_secret: string;
-//   };
+type StripeResponse = {
+  payload: {
+    client_secret: string;
+  };
+};
+
+const PaymentForm: React.FC = () => {
+  const { data: session } = useSession();
+  const stripe = useStripe();
+  const elements = useElements();
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe) return;
+
+    const { error: submitError } = await elements!.submit();
+    if (submitError) {
+      console.log(submitError);
+      return;
+    }
+
+    const res = await BrowserRest.post<StripeResponse>(
+      "/payment",
+      {
+        total: 200,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${session?.user?.token}`,
+        },
+      }
+    );
+
+    const { client_secret: clientSecret } = await res.data.payload;
+
+    if (!elements) return;
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: `${process.env.NEXT_API_URL}/carts/${session?.user?.cart}/purchase`,
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-[600px] h-[300px] text-center p-10 bg-white"
+    >
+      <span className="text-s w-full h-[30px] p-2  uppercase font-bold">
+        Ingrese los datos de su tarjeta
+      </span>
+      <PaymentElement />
+      <button className="text-s bg-font w-full rounded text-white p-2 m-5 uppercase shadow-xl font-bold max-w-[200px]">
+        Comprar
+      </button>
+    </form>
+  );
+};
+
+export default PaymentForm;
+
+// useEffect(() => {
+//   if (session) {
+//     const getClientSecret = async () => {
+//       const response = await BrowserRest.post<StripeResponse>(
+//         "/payment",
+//         {
+//           total: 200,
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${session.user?.token}`,
+//           },
+//         }
+//       );
+
+//       if (response.status === RESPONSE_STATUS.SUCCESS) {
+//         setStripeSecret(response.data.payload.client_secret);
+//       }
+//     };
+//     getClientSecret();
+//   }
+// }, [session]);
+
+// const makePayment = async (e: FormEvent<HTMLFormElement>) => {
+//   e.preventDefault();
+
+//   const el = elements?.getElement(CardElement);
+//   console.log(el);
+
+//   const payload = await stripe?.confirmCardPayment(stripeScret!, {
+//     payment_method: {
+//       card: elements?.getElement(CardElement)!,
+//     },
+//   });
+
+//   if (payload?.paymentIntent?.status === STRIPE_STATUS.SUCCESS) {
+//     router.push(`/carts/${session?.user?.cart}/purchase`);
+//   } else {
+//     router.push(`/carts/${session?.user?.cart}`);
+//   }
 // };
 
 // const CARD_ELEMENT_OPTIONS = {
@@ -34,90 +138,6 @@ import { useRouter } from "next/router";
 //   },
 // };
 
-const PaymentForm: React.FC = () => {
-  const { data: session } = useSession();
-  // const [stripeScret, setStripeSecret] = useState<string>();
-  const stripe = useStripe();
-  const elements = useElements();
-  const router = useRouter();
+// <CardElement options={CARD_ELEMENT_OPTIONS} />
 
-  // useEffect(() => {
-  //   if (session) {
-  //     const getClientSecret = async () => {
-  //       const response = await BrowserRest.post<StripeResponse>(
-  //         "/payment",
-  //         {
-  //           total: 200,
-  //         },
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${session.user?.token}`,
-  //           },
-  //         }
-  //       );
-
-  //       if (response.status === RESPONSE_STATUS.SUCCESS) {
-  //         setStripeSecret(response.data.payload.client_secret);
-  //       }
-  //     };
-  //     getClientSecret();
-  //   }
-  // }, [session]);
-
-  // const makePayment = async (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-
-  //   const el = elements?.getElement(CardElement);
-  //   console.log(el);
-
-  //   const payload = await stripe?.confirmCardPayment(stripeScret!, {
-  //     payment_method: {
-  //       card: elements?.getElement(CardElement)!,
-  //     },
-  //   });
-
-  //   if (payload?.paymentIntent?.status === STRIPE_STATUS.SUCCESS) {
-  //     router.push(`/carts/${session?.user?.cart}/purchase`);
-  //   } else {
-  //     router.push(`/carts/${session?.user?.cart}`);
-  //   }
-  // };
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
-
-    if (!stripe || !elements) {
-      // Stripe.js hasn't yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    const { error } = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: "https://example.com/order/123/complete",
-      },
-    });
-  };
-
-  return (
-    <form
-      onSubmit={handleSubmit}
-      className="w-[600px] h-[300px] text-center p-10 bg-white"
-    >
-      <span className="text-s w-full h-[30px] p-2  uppercase font-bold">
-        Ingrese los datos de su tarjeta
-      </span>
-      {/* <CardElement options={CARD_ELEMENT_OPTIONS} /> */}
-      <PaymentElement />
-      <button className="text-s bg-font w-full rounded text-white p-2 m-5 uppercase shadow-xl font-bold max-w-[200px]">
-        Comprar
-      </button>
-    </form>
-  );
-};
-
-export default PaymentForm;
+// const [stripeScret, setStripeSecret] = useState<string>();
